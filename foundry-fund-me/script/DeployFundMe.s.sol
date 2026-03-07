a5572eb0c038991a2eb79de114ee6ff7c10d2b24
Márcio Júnior <marcio.mj70@gmail.com>
2026-03-07 17:48:30 -0300

    feat: add cheaper version of withdraw function

--- foundry-fund-me/script/DeployFundMe.s.sol
+++ foundry-fund-me/script/DeployFundMe.s.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.30;
 
-import {Script} from "forge-std/Script.sol";
+import {Script, console} from "forge-std/Script.sol";
 import {FundMe} from "../src/FundMe.sol";
 import {HelperConfig} from "./HelperConfig.s.sol";
 
@@ -13,7 +13,27 @@ contract DeployFundMe is Script {
         vm.startBroadcast();
         FundMe fundMe = new FundMe(ethUsdPriceFeed);
         vm.stopBroadcast();
+
+        printStorageData(address(fundMe));
+        printFirstArrayElement(address(fundMe));
+
         return fundMe;
+    }
+
+    function printStorageData(address contractAddress) public view {
+        for (uint256 i = 0; i < 10; i++) {
+            bytes32 value = vm.load(contractAddress, bytes32(i));
+            console.log("Value at location", i, ":");
+            console.logBytes32(value);
+        }
+    }
+
+    function printFirstArrayElement(address contractAddress) public view {
+        bytes32 arrayStorageSlotLength = bytes32(uint256(2));
+        bytes32 firstElementStorageSlot = keccak256(abi.encode(arrayStorageSlotLength));
+        bytes32 value = vm.load(contractAddress, firstElementStorageSlot);
+        console.log("First element in array:");
+        console.logBytes32(value);
     }
 }
 
