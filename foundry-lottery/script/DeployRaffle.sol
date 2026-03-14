1f68ba7c0a1fbce0820849c8c6756db59dfbb248
Márcio Júnior <marcio.mj70@gmail.com>
2026-03-14 13:55:16 -0300

    feat: add scripts to deploy contract to multiple networks

---
+++ foundry-lottery/script/DeployRaffle.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.30;
+
+import {Script} from "forge-std/Script.sol";
+import {Raffle} from "src/Raffle.sol";
+import {HelperConfig} from "script/HelperConfig.sol";
+
+contract DeployRaffle is Script {
+    function run() external returns (Raffle) {}
+
+    function deployContract() public returns (Raffle, HelperConfig) {
+        HelperConfig helperConfig = new HelperConfig();
+        // local -> deploy mocks, get local config
+        // sepolia -> get sepolia config
+        HelperConfig.NetworkConfig memory config = helperConfig.getConfig();
+
+        vm.startBroadcast();
+        Raffle raffle = new Raffle(
+            config.entranceFee,
+            config.interval,
+            config.vrfCoordinator,
+            config.gasLane,
+            config.subscriptionId,
+            config.callbackGasLimit
+        );
+        vm.stopBroadcast();
+
+        return (fundMe, helperConfig);
+    }
+}
